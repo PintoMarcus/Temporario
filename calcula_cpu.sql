828c25159e8fc55c47f6f5910c25e562eaf6beee
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2023-09-24 16:56:26 -0300

    Update calcula_cpu.sql

--- calcula_cpu.sql
+++ calcula_cpu.sql
@@ -20,3 +20,33 @@ FROM
     [dbo].[tbx44_base_sql_cpu] AS A
 INNER JOIN
     [dbo].[tbx44_base_sql_cpu_database] AS B ON DATEADD(MINUTE, DATEDIFF(MINUTE, 0, A.dtcoleta), 0) = DATEADD(MINUTE, DATEDIFF(MINUTE, 0, B.dtcoleta), 0);
+
+
+**********************
+
+SELECT
+    runtime,
+    database_name,
+    allocated_memory_gb,
+    CASE 
+        WHEN DATEPART(WEEKDAY, runtime) BETWEEN 2 AND 6 -- Segunda a Sexta (2 a 6)
+        AND CAST(runtime AS TIME) BETWEEN '07:00:00.000' AND '21:00:00.000' -- Horário comercial
+        THEN 'Sim'
+        ELSE 'Não'
+    END AS [hora-comercial]
+FROM contador_memoria;
+
+
+-- versão 2008
+SELECT
+    runtime,
+    database_name,
+    allocated_memory_gb,
+    CASE 
+        WHEN DATEPART(WEEKDAY, runtime) BETWEEN 2 AND 6 -- Segunda a Sexta (2 a 6)
+            AND CONVERT(TIME, runtime) BETWEEN '07:00:00' AND '21:00:00' -- Horário comercial
+            THEN 'Sim'
+        ELSE 'Não'
+    END AS [hora-comercial]
+FROM contador_memoria;
+
