477dc9bd004ad68701c4bda99f37e92686a71a9c
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2023-09-28 14:22:18 -0300

    Create table-size-com-indice-e-dados.sql

---
+++ table-size-com-indice-e-dados.sql
@@ -0,0 +1,21 @@
+USE SeuBancoDeDados; -- Substitua pelo nome do seu banco de dados
+GO
+
+DECLARE @Tabela NVARCHAR(128) = 'SuaTabela'; -- Substitua pelo nome da sua tabela
+
+-- Obter informações do espaço da tabela
+DBCC UPDATEUSAGE (0); -- Atualiza as informações de uso do banco de dados
+SELECT 
+    t.NAME AS TableName,
+    p.rows AS NumeroDeLinhas,
+    SUM(a.total_pages) * 8 AS EspacoTotalKB,
+    SUM(a.used_pages) * 8 AS EspacoUsadoKB,
+    (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS EspacoLivreKB,
+    SUM(CASE WHEN i.index_id <= 1 THEN a.used_pages ELSE 0 END) * 8 AS EspacoUsadoPorDadosKB,
+    SUM(CASE WHEN i.index_id > 1 THEN a.used_pages ELSE 0 END) * 8 AS EspacoUsadoPorIndicesKB
+FROM sys.tables t
+INNER JOIN sys.indexes i ON t.OBJECT_ID = i.OBJECT_ID
+INNER JOIN sys.partitions p ON i.OBJECT_ID = p.OBJECT_ID AND i.index_id = p.index_id
+INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
+WHERE t.NAME = @Tabela
+GROUP BY t.NAME, p.rows;
