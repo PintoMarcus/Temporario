a65b3c0fbf846f261619ded4efccdbe958b9bcdf
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2023-10-05 11:02:42 -0300

    Update pg-pwd-hash.sql

--- pg-pwd-hash.sql
+++ pg-pwd-hash.sql
@@ -4,3 +4,9 @@ SELECT password FROM pg_shadow WHERE usename = 'seu_usuario';
 -- altera o hash pwd do usuário na instância de destino.
 ALTER USER seu_usuario PASSWORD 'hash_da_senha_da_instancia_01';
 
+--
+--- Cria usuário com senha
+SELECT 'CREATE USER ' || usename || ' WITH PASSWORD ''' || passwd || ''';'
+FROM pg_shadow
+WHERE usename IN ('user-01','usr02');
+
