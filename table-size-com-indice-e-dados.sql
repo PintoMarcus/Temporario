012804fcd11ad850079875178d6d9029b87d75f6
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2023-10-13 16:38:59 -0300

    Update table-size-com-indice-e-dados.sql

--- table-size-com-indice-e-dados.sql
+++ table-size-com-indice-e-dados.sql
@@ -19,3 +19,52 @@ INNER JOIN sys.partitions p ON i.OBJECT_ID = p.OBJECT_ID AND i.index_id = p.inde
 INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
 WHERE t.NAME = @Tabela
 GROUP BY t.NAME, p.rows;
+
+
+/*++++++++++++++++++++++++*/
+-- USANDO a PORTA do BABELFISH
+-- Crie uma tabela temporária para armazenar os resultados
+CREATE TABLE #TableSizes
+(
+    TableName NVARCHAR(128),
+    RowCount INT,
+    ReservedSize VARCHAR(50),
+    DataSize VARCHAR(50),
+    IndexSize VARCHAR(50),
+    UnusedSize VARCHAR(50)
+)
+
+-- Declare variáveis para conter o nome das tabelas
+DECLARE @TableName NVARCHAR(128)
+
+-- Declare um cursor para percorrer as tabelas
+DECLARE tableCursor CURSOR FOR
+SELECT name
+FROM sys.tables
+
+-- Itera pelas tabelas
+OPEN tableCursor
+FETCH NEXT FROM tableCursor INTO @TableName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    INSERT INTO #TableSizes
+    EXEC sp_spaceused @TableName
+
+    FETCH NEXT FROM tableCursor INTO @TableName
+END
+
+-- Fecha o cursor
+CLOSE tableCursor
+DEALLOCATE tableCursor
+
+-- Seleciona os resultados
+SELECT
+    TableName AS 'Nome da Tabela',
+    RowCount AS 'Número de Linhas',
+    DataSize AS 'Tamanho dos Dados',
+    IndexSize AS 'Tamanho dos Índices'
+FROM #TableSizes
+
+-- Limpa a tabela temporária
+DROP TABLE #TableSizes
