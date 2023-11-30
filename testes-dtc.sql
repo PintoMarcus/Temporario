5ebb372500b6415e2589e27c1e2d7717bb267002
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2023-11-30 11:06:10 -0300

    Create testes-dtc.sql

---
+++ testes-dtc.sql
@@ -0,0 +1,24 @@
+-- TESTES DTC
+
+--Verificar Configurações do DTC: O resultado deve ser 1, o que significa que consultas distribuídas ad hoc são permitidas.
+EXEC sp_configure 'Ad Hoc Distributed Queries';
+
+
+-- Verificar Se o DTC Está Ativo: (Se houver transações ativas, o DTC está em execução.)
+
+SELECT * FROM sys.dm_tran_active_transactions;
+
+
+-- Teste de Transações Distribuídas: (Teste Básico com COMMIT)
+BEGIN DISTRIBUTED TRANSACTION;
+-- Seu código de transação aqui
+COMMIT;
+
+-- Teste de Transações Distribuídas: (Teste Básico com ROLLBACK)
+BEGIN DISTRIBUTED TRANSACTION;
+-- Seu código de transação aqui
+ROLLBACK;
+
+
+-- Verificar Transações Distribuídas Ativas: ()
+SELECT * FROM sys.dm_tran_active_transactions WHERE is_local = 0;
