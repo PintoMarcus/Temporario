ff6d2664647aafb30faf0546e919206001b79a89
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2023-11-30 13:06:30 -0300

    Update testes-dtc.sql

--- testes-dtc.sql
+++ testes-dtc.sql
@@ -25,7 +25,7 @@ SELECT * FROM sys.dm_tran_active_transactions WHERE is_local = 0;
 
 
 
---- testando com openquery
+--- testando com openquery 001
 BEGIN TRANSACTION;
 
 -- Substitua 'RemoteServer' pelo nome do servidor remoto
@@ -35,6 +35,28 @@ SET @Sql = N'SELECT * FROM DatabaseName.SchemaName.TableName';
 
 -- Executa a transação distribuída usando OPENQUERY
 EXEC ('SELECT * FROM OPENQUERY(RemoteServer, ''' + @Sql + ''')');
+
+-- Commit ou Rollback da transação distribuída
+COMMIT; -- ou ROLLBACK;
+
+
+-- testando com openquery 002
+BEGIN DISTRIBUTED TRANSACTION;
+
+-- Substitua 'SRV_002.meudomini.abc' pelo nome do servidor remoto completo
+-- Substitua 'sch1' pelo esquema no servidor remoto
+-- Substitua 'mtb1' pela tabela no servidor remoto
+DECLARE @Sql NVARCHAR(MAX);
+SET @Sql = N'select top 10 * from [sch1].[mtb1]';
+
+-- Executa a transação distribuída usando OPENQUERY
+EXEC ('SELECT * INTO #TempTable FROM OPENQUERY([SRV_002.meudomini.abc], ''' + @Sql + ''')');
+
+-- Espera por 15 segundos
+WAITFOR DELAY '00:00:15';
+
+-- Seleciona os resultados da tabela temporária
+SELECT * FROM #TempTable;
 
 -- Commit ou Rollback da transação distribuída
 COMMIT; -- ou ROLLBACK;
