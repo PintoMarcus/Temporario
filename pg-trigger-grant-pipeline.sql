766137ec995989afd2b65e6ed124ac1c0a8b422f
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2024-03-01 09:31:36 -0300

    Add files via upload

---
+++ pg-trigger-grant-pipeline.sql
@@ -0,0 +1,80 @@
+-- função para grant nas roles _dbo para o usuário pipeline
+CREATE OR REPLACE FUNCTION grant_to_pipeline()
+RETURNS event_trigger
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    role_name text;
+BEGIN
+    SELECT current_user INTO role_name;
+
+    -- Verifica se o nome da role atende aos critérios
+    IF role_name ~ '_dbo$' THEN
+        -- Concede a permissão à role pipeline
+        EXECUTE 'GRANT ' || role_name || ' TO pipeline';
+    END IF;
+END;
+$$;
+
+
+
+-- trigger que dispara a função.
+CREATE EVENT TRIGGER grant_to_pipeline_trigger
+ON ddl_command_end
+WHEN TAG IN ('CREATE ROLE')
+EXECUTE FUNCTION grant_to_pipeline();
+
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++*/
+-- função semelhante a anterior , mas que irá dar o grant se para qualquer role criada desde que exista com schema com mesmo nome
+CREATE OR REPLACE FUNCTION grant_to_pipeline()
+RETURNS event_trigger
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    role_name text;
+    schema_exists boolean;
+BEGIN
+    SELECT current_user INTO role_name;
+
+    -- Verifica se o schema com o mesmo nome da role existe
+    EXECUTE 'SELECT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = $1)' INTO schema_exists
+    USING role_name;
+
+    -- Se o schema existe, concede a permissão à role pipeline
+    IF schema_exists THEN
+        EXECUTE 'GRANT ' || role_name || ' TO pipeline';
+    END IF;
+END;
+$$;
+
+
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++*/
+-- função semelhante a anterior , mas que irá dar o grant se para qualquer role criada desde que seja criado um schema com mesmo nome
+CREATE OR REPLACE FUNCTION grant_to_pipeline()
+RETURNS event_trigger
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    role_name text;
+BEGIN
+    SELECT current_user INTO role_name;
+
+    -- Verifica se o schema com o mesmo nome da role existe
+    IF EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = role_name) THEN
+        -- Se o schema existe, concede a permissão à role pipeline
+        EXECUTE 'GRANT ' || role_name || ' TO pipeline';
+    END IF;
+END;
+$$;
+
+CREATE EVENT TRIGGER grant_pipeline_trigger
+ON ddl_command_end
+WHEN TAG IN ('CREATE ROLE')
+EXECUTE FUNCTION grant_to_pipeline();
+
+
+
