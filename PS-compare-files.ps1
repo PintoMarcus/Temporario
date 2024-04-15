5eff8e6c4613731d1ca7b4c0b6a47d114e8233e8
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2024-04-15 10:42:14 -0300

    Update PS-compare-files.ps1

--- PS-compare-files.ps1
+++ PS-compare-files.ps1
@@ -123,4 +123,71 @@ if (Test-Path $caminhoLista) {
     Write-Host "O arquivo $caminhoLista não foi encontrado."
 }
 
-###################################
+############################################################################################################################################
+######################################################################
+# compare removendo zeros e vírgulas
+######################################################################
+# Caminho do arquivo lista.txt
+$caminhoLista = "C:\temp\compare-origem\lista.txt"
+# Caminho do arquivo de resultado
+$caminhoResultado = "C:\temp\compare-origem\resultado.txt"
+
+# Verifica se o arquivo de lista existe
+if (Test-Path $caminhoLista) {
+    # Leitura do arquivo lista.txt
+    $linhas = Get-Content $caminhoLista
+
+    # Array para armazenar os resultados
+    $resultados = @()
+
+    # Loop através de cada linha do arquivo lista.txt
+    foreach ($linha in $linhas) {
+        # Dividindo a linha em origem e destino
+        $arquivos = $linha -split ","
+        $origem = $arquivos[0].Trim()
+        $destino = $arquivos[1].Trim()
+
+        # Calcular o hash MD5 dos arquivos de origem e destino
+        $hashOrigem = (Get-FileHash -Path $origem -Algorithm MD5).Hash
+        $hashDestino = (Get-FileHash -Path $destino -Algorithm MD5).Hash
+
+        # Verifica se os hashes são iguais
+        if ($hashOrigem -eq $hashDestino) {
+            $resultado = "$origem e $destino são IGUAIS"
+        } else {
+            # Lê o conteúdo dos arquivos
+            $conteudoOrigem = Get-Content $origem
+            $conteudoDestino = Get-Content $destino
+
+            # Remove todos os caracteres "0" e ","
+            $conteudoOrigemSemZerosVirgulas = $conteudoOrigem -replace '[0,]', ''
+            $conteudoDestinoSemZerosVirgulas = $conteudoDestino -replace '[0,]', ''
+
+            # Verificar se os arquivos são iguais após a remoção de zeros e vírgulas
+            if ($conteudoOrigemSemZerosVirgulas -eq $conteudoDestinoSemZerosVirgulas) {
+                $resultado = "$origem e $destino são IGUAIS com DECIMAL"
+            } else {
+                # Escreve os resultados intermediários no arquivo de resultado
+                $resultado = "$origem e $destino são DIFERENTES"
+                $resultados += $resultado
+                
+                # Remove os zeros e vírgulas apenas se os arquivos forem diferentes
+                $conteudoOrigemSemZerosVirgulas | Set-Content $origem -Force
+                $conteudoDestinoSemZerosVirgulas | Set-Content $destino -Force
+
+                Write-Host "Zeros e vírgulas removidos dos arquivos diferentes: $origem e $destino"
+            }
+        }
+
+        # Adicionando o resultado ao array
+        $resultados += $resultado
+    }
+
+    # Escreve os resultados finais no arquivo de resultado
+    $resultados | Out-File -FilePath $caminhoResultado -Encoding utf8
+
+    Write-Host "Processo concluído. Resultados salvos em $caminhoResultado"
+} else {
+    Write-Host "O arquivo $caminhoLista não foi encontrado."
+}
+
