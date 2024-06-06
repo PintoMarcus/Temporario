19c4e2cfb75d1fb4ffbb760f3b644628256237f4
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2024-06-06 09:18:00 -0300

    Update PG-Default-Privileges.sql

--- PG-Default-Privileges.sql
+++ PG-Default-Privileges.sql
@@ -84,33 +84,35 @@ CREATE OR REPLACE FUNCTION grant_permissions(
     target_role TEXT,
     target_user TEXT
 )
-RETURNS VOID AS $$
+RETURNS TABLE(command TEXT) 
+SECURITY DEFINER    
+AS $$
 DECLARE
     rec RECORD;
 BEGIN
     -- Grant all privileges on the schema
-    EXECUTE format('GRANT ALL ON SCHEMA %I TO %I WITH GRANT OPTION;', target_schema, target_user);
+    RETURN QUERY EXECUTE format('GRANT ALL ON SCHEMA %I TO %I WITH GRANT OPTION;', target_schema, target_user);
 
     -- Alter default privileges for tables
-    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT REFERENCES, TRIGGER ON TABLES TO %I WITH GRANT OPTION;', target_role, target_schema, target_user);
+    RETURN QUERY EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT REFERENCES, TRIGGER ON TABLES TO %I WITH GRANT OPTION;', target_role, target_schema, target_user);
 
     -- Alter default privileges for sequences
-    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT SELECT, USAGE ON SEQUENCES TO %I WITH GRANT OPTION;', target_role, target_schema, target_user);
+    RETURN QUERY EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT SELECT, USAGE ON SEQUENCES TO %I WITH GRANT OPTION;', target_role, target_schema, target_user);
 
     -- Alter default privileges for functions
-    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT EXECUTE ON FUNCTIONS TO %I WITH GRANT OPTION;', target_role, target_schema, target_user);
+    RETURN QUERY EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT EXECUTE ON FUNCTIONS TO %I WITH GRANT OPTION;', target_role, target_schema, target_user);
 
     -- Alter default privileges for types
-    EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT USAGE ON TYPES TO %I WITH GRANT OPTION;', target_role, target_schema, target_user);
+    RETURN QUERY EXECUTE format('ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA %I GRANT USAGE ON TYPES TO %I WITH GRANT OPTION;', target_role, target_schema, target_user);
 
     -- Grant specific privileges on all current tables
-    EXECUTE format('GRANT REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA %I TO %I WITH GRANT OPTION;', target_schema, target_user);
+    RETURN QUERY EXECUTE format('GRANT REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA %I TO %I WITH GRANT OPTION;', target_schema, target_user);
 
     -- Grant specific privileges on all current sequences
-    EXECUTE format('GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA %I TO %I WITH GRANT OPTION;', target_schema, target_user);
+    RETURN QUERY EXECUTE format('GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA %I TO %I WITH GRANT OPTION;', target_schema, target_user);
 
     -- Grant specific privileges on all current functions
-    EXECUTE format('GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA %I TO %I WITH GRANT OPTION;', target_schema, target_user);
+    RETURN QUERY EXECUTE format('GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA %I TO %I WITH GRANT OPTION;', target_schema, target_user);
 
     -- Grant usage on all types in the schema
     FOR rec IN 
@@ -119,7 +121,7 @@ BEGIN
         JOIN pg_namespace ON pg_type.typnamespace = pg_namespace.oid 
         WHERE nspname = target_schema
     LOOP
-        EXECUTE format('GRANT USAGE ON TYPE %I.%I TO %I;', rec.nspname, rec.typname, target_user);
+        RETURN QUERY EXECUTE format('GRANT USAGE ON TYPE %I.%I TO %I;', rec.nspname, rec.typname, target_user);
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
