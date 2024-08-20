10836b72def608e579b2a7502878825512cc53f7
Marcus Pinto - DBA <122027849+PintoMarcus@users.noreply.github.com>
2024-08-20 10:01:56 -0300

    Update SQL-Cluster-information.sql

--- SQL-Cluster-information.sql
+++ SQL-Cluster-information.sql
@@ -3,42 +3,38 @@
 SELECT 
     cs.virtual_server_name AS ClusterName,
     cs.instance_name AS SQLInstanceName,
-    n.node_name AS NodeName,
-    n.status_description AS NodeStatus,
-    cs.active_start_date AS InstallDate
+    cn.node_name AS NodeName,
+    cn.status_description AS NodeStatus
 FROM 
-    sys.dm_os_cluster_nodes AS n
+    sys.dm_os_cluster_nodes AS cn
 INNER JOIN 
     sys.dm_os_cluster_servers AS cs 
-    ON cs.node_name = n.node_name;
+    ON cs.node_name = cn.node_name;
 
 -- Verifica o status do cluster
 SELECT 
     cluster_name, 
     quorum_type_desc, 
-    quorum_state_desc, 
-    NumberOfNodes, 
-    NumberOfVotingNodes 
+    quorum_state_desc
 FROM 
     sys.dm_hadr_cluster;
-
 
 
 --002
 -- Verifica o status de cada instância do SQL Server no cluster
 SELECT 
-    serverproperty('MachineName') AS NodeName,
-    serverproperty('IsClustered') AS IsClustered, 
-    serverproperty('ComputerNamePhysicalNetBIOS') AS ActiveNode,
-    serverproperty('InstanceName') AS InstanceName,
-    serverproperty('Edition') AS Edition,
-    serverproperty('ProductVersion') AS ProductVersion;
+    SERVERPROPERTY('MachineName') AS NodeName,
+    SERVERPROPERTY('IsClustered') AS IsClustered, 
+    SERVERPROPERTY('ComputerNamePhysicalNetBIOS') AS ActiveNode,
+    SERVERPROPERTY('InstanceName') AS InstanceName,
+    SERVERPROPERTY('Edition') AS Edition,
+    SERVERPROPERTY('ProductVersion') AS ProductVersion;
 
 
 --003
 -- Verifica a configuração de Database Mirroring
 SELECT 
-    db_name(database_id) AS DatabaseName,
+    DB_NAME(database_id) AS DatabaseName,
     mirroring_state_desc,
     mirroring_role_desc,
     mirroring_partner_name,
@@ -55,8 +51,6 @@ SELECT
     ar.replica_server_name AS ReplicaServerName,
     ar.availability_mode_desc AS AvailabilityMode,
     ar.failover_mode_desc AS FailoverMode,
-    ar.primary_role_allow_connections_desc AS PrimaryRoleConnections,
-    ar.secondary_role_allow_connections_desc AS SecondaryRoleConnections,
     ars.role_desc AS CurrentRole,
     ars.connected_state_desc AS ConnectionState
 FROM 
@@ -71,7 +65,7 @@ JOIN
 -- Verifica o status dos bancos de dados nos Grupos de Disponibilidade
 SELECT 
     ag.name AS AGName,
-    db_name(drs.database_id) AS DatabaseName,
+    DB_NAME(drs.database_id) AS DatabaseName,
     drs.synchronization_state_desc AS SyncState,
     drs.is_suspended AS IsSuspended,
     drs.recovery_health_desc AS RecoveryState,
@@ -98,21 +92,16 @@ FROM
 -- Verifica os assinantes da replicação
 SELECT 
     s.srvname AS SubscriberServer,
-    d.name AS SubscriberDB,
-    a.subscriber_id,
-    a.subscriber_db,
+    a.subscriber_db AS SubscriberDB,
     a.subscription_type,
     a.subscription_status,
-    a.last_sync_status,
     a.last_sync_summary,
     a.last_sync_time
 FROM 
     distribution.dbo.MSsubscriptions AS a
 JOIN 
-    sysservers AS s 
-    ON a.subscriber_id = s.srvid
-JOIN 
-    sys.databases AS d 
-    ON a.subscriber_db = d.name;
+    sys.servers AS s 
+    ON a.subscriber_id = s.server_id;
+
 
 
